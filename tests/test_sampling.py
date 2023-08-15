a715e2173488cf9991a2d39857d8f1bdfde70b6c
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-08-15 19:30:42 +0200

    Added input tests

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -14,11 +14,48 @@ data['x_1 * x_2'] = data['x_1']*data['x_2']
 
 data['y'] = 3*data['x_1'] - 20*data['x_2'] - data['x_3'] - 5*data['x_1 * x_2'] + 13 + 1*np.random.randn(N)
 
+initial_values = {'x_1': 1,
+                  'x_2': 2,
+                  'x_3': 3,
+                  'x_1 * x_2': 4}
+initial_values_intercept = 5
+
+sigma2_sample_size = 5
+sigma2_variance = 10
+
+prior = {'x_1': {'mean': 0,
+                 'variance': 1e6},
+         'x_2': {'mean': 0,
+                 'variance': 1e6},
+         'x_3': {'mean': 0,
+                 'variance': 1e6},
+         'x_1 * x_2': {'mean': 0,
+                       'variance': 1e6},
+         'intercept': {'mean': 0,
+                       'variance': 1e6},
+         'sigma2': {'shape': sigma2_sample_size,
+                    'scale': sigma2_sample_size*sigma2_variance}}
+
 
 @mark.set_input
 def test_set_data(sampler):
     sampler.set_data(data = data,
                      y_name = 'y')
 
-    assert data.equals(sampler.data)
-    assert 'y' == sampler.y_name
+    assert sampler.data.equals(data)
+    assert sampler.y_name == 'y'
+
+@mark.set_input
+def test_set_initial_values(sampler):
+    sampler.set_initial_values(initial_values = initial_values,
+                               initial_value_intercept = initial_values_intercept)
+
+    assert sampler.initial_values == initial_values
+    assert sampler.initial_values_intercept == initial_values_intercept
+
+@mark.set_input
+def test_set_prior(sampler):
+    sampler.set_prior(prior = prior)
+
+    assert sampler.prior == prior
+
