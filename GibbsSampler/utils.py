e97f575a0eba28738a5c55202d550d0b4e2e0727
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-08-27 23:10:47 +0200

    Added utils' input error check

--- GibbsSampler/utils.py
+++ GibbsSampler/utils.py
@@ -15,6 +15,13 @@ def flatten_matrix(matrix: np.ndarray) -> np.ndarray:
     numpy.ndarray
         Flatten array with ``M*N`` elements.
 
+    Raises
+    ------
+    TypeError
+        If ``matrix`` is not a ``numpy.ndarray``.
+    ValueError
+        If ``matrix`` is an empty ``numpy.ndarray``.
+
     Examples
     --------
     >>> import numpy
@@ -26,6 +33,12 @@ def flatten_matrix(matrix: np.ndarray) -> np.ndarray:
     >>> flatten_matrix(a)
     >>> array([1, 2, 3, 4, 5, 6])
     """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("Parameter 'matrix' must be an instance of 'numpy.ndarray'")
+
+    if matrix.size == 0:
+        raise ValueError(f"Parameter 'matrix' is empty")
+
     return np.asarray(matrix).reshape(-1)
 
 
@@ -46,6 +59,14 @@ def matrices_to_frame(matrices_dict: dict) -> pd.DataFrame:
         Reorganized matrices frame. Matrices are organized in a ``pandas.DataFrame``, one for each columns. The
         length of the frame is ``M*N``.
 
+    Raises
+    ------
+    TypeError
+        - If ``matrices_dict`` is not a ``dict``,
+        - if a ``matrices_dict`` value is not a ``numpy.ndarray``.
+    ValueError
+        If a ``matrices_dict`` value is an empty ``numpy.ndarray``.
+
     Examples
     --------
     >>> import numpy
@@ -61,4 +82,14 @@ def matrices_to_frame(matrices_dict: dict) -> pd.DataFrame:
     >>> 4  5  11
     >>> 5  6  12
     """
+    if not isinstance(matrices_dict, dict):
+        raise TypeError(f"Parameter 'matrices_dict' must be a dictionary")
+
+    for matrix_name, matrix in matrices_dict.items():
+        if not isinstance(matrix, np.ndarray):
+            raise TypeError(f"Matrix '{matrix_name}' must be an instance of 'numpy.ndarray'")
+
+        if matrix.size == 0:
+            raise ValueError(f"Matrix '{matrix_name}' is empty")
+
     return pd.DataFrame({col: flatten_matrix(matrix) for col, matrix in matrices_dict.items()})
