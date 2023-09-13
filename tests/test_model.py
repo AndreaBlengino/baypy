86ed46405a4dd0f1b0f3ff7cb78961bac4d36307
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-09-14 01:37:59 +0200

    TST: use hypothesis for more robust model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,72 @@
+from baypy.model import LinearModel
+from hypothesis import given, settings, HealthCheck
+from hypothesis.strategies import composite, integers, floats
 import pandas as pd
 import numpy as np
 from pytest import mark, raises
+
+
+@composite
+def model_set_up(draw):
+    n_regressors = draw(integers(min_value = 0, max_value = 5))
+    n_data_points = draw(integers(min_value = 20, max_value = 100))
+    regressors_minimum = draw(floats(min_value = -1000, max_value = 0))
+    regressors_maximum = draw(floats(min_value = 1, max_value = 1000))
+    regressors_parameters_minimum = draw(floats(min_value = -100, max_value = -1))
+    regressors_parameters_maximum = draw(floats(min_value = 1, max_value = 100))
+    n_samples = draw(integers(min_value = 20, max_value = 1000))
+    n_chains = draw(integers(min_value = 1, max_value = 5))
+
+    regressors_names = np.random.choice(list('abcdefghijklmnopqrstuvxyz'), n_regressors, replace = False).tolist()
+    data = pd.DataFrame({regressor_name: np.random.uniform(low = regressors_minimum,
+                                                           high = regressors_maximum,
+                                                           size = n_data_points)
+                         for regressor_name in regressors_names})
+    data['intercept'] = data['intercept'] = np.ones(n_data_points)
+    regressors_parameters = np.random.uniform(low = regressors_parameters_minimum,
+                                              high = regressors_parameters_maximum,
+                                              size = n_regressors + 1)
+    response_variable = np.random.choice([name for name in list('abcdefghijklmnopqrstuvxyz')
+                                          if name not in data.columns], 1).tolist()[0]
+    data[response_variable] = (data*regressors_parameters).sum(axis = 1) + \
+                              np.random.normal(loc = 0, scale = 1, size = n_data_points)
+    data.drop(columns = ['intercept'], inplace = True)
+
+    priors = {regressor_name: {'mean': 0, 'variance': 1e14} for regressor_name in regressors_names}
+    priors['intercept'] = {'mean': 0, 'variance': 1e14}
+    priors['variance'] = {'shape': 1, 'scale': 1e-14}
+
+    posteriors = {posterior_name: np.random.randn(n_samples, n_chains) for posterior_name in priors.keys()}
+    posteriors['variance'] = np.abs(posteriors['variance'])
+
+    predictors = {predictor: np.random.uniform(low = regressors_parameters_minimum,
+                                               high = regressors_parameters_maximum,
+                                               size = 1)[0]
+                  for predictor in priors.keys() if predictor not in ['intercept', 'variance']}
+
+    return {'data': data,
+            'response_variable': response_variable,
+            'priors': priors,
+            'posteriors': posteriors,
+            'n_samples': n_samples,
+            'n_chains': n_chains,
+            'predictors': predictors}
+
+
+@composite
+def likelihood_data(draw):
+    n_data_points = draw(integers(min_value = 20, max_value = 100))
+    mean_minimum = draw(floats(min_value = -1000, max_value = 0))
+    mean_maximum = draw(floats(min_value = 1, max_value = 1000))
+    variance_minimum = draw(floats(min_value = 1e-10, max_value = 10))
+    variance_maximum = draw(floats(min_value = 11, max_value = 1000))
+
+    return pd.DataFrame({'mean': np.random.uniform(low = mean_minimum,
+                                                   high = mean_maximum,
+                                                   size = n_data_points),
+                         'variance': np.random.uniform(low = variance_minimum,
+                                                       high = variance_maximum,
+                                                       size = n_data_points)})
 
 
 @mark.model
@@ -8,10 +74,15 @@ class TestModelData:
 
 
     @mark.genuine
-    def test_property(self, empty_model, general_testing_data):
-        empty_model.data = general_testing_data['data']
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_property(self, model_set_up):
+        model = LinearModel()
+        model.data = model_set_up['data']
 
-        assert empty_model.data.equals(general_testing_data['data'])
+        assert isinstance(model.data, pd.DataFrame)
+        assert model.data.equals(model_set_up['data'])
+        assert not model.data.empty
 
 
     @mark.error
@@ -31,10 +102,14 @@ class TestModelResponseVariable:
 
 
     @mark.genuine
-    def test_property(self, empty_model, general_testing_data):
-        empty_model.response_variable = general_testing_data['response_variable']
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_property(self, model_set_up):
+        model = LinearModel()
+        model.response_variable = model_set_up['response_variable']
 
-        assert empty_model.response_variable == general_testing_data['response_variable']
+        assert isinstance(model.response_variable, str)
+        assert model.response_variable == model_set_up['response_variable']
 
 
     @mark.error
@@ -48,21 +123,26 @@ class TestModelPriors:
 
 
     @mark.genuine
-    def test_property(self, empty_model, general_testing_data):
-        empty_model.priors = general_testing_data['priors']
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_property(self, model_set_up):
+        model = LinearModel()
+        model.priors = model_set_up['priors']
 
-        assert empty_model.priors == general_testing_data['priors']
-        assert 'intercept' in empty_model.priors.keys()
-        assert 'variance' in empty_model.priors.keys()
-        assert all(['mean' in regressor_data.keys() for regressor, regressor_data in empty_model.priors.items()
+        assert isinstance(model.priors, dict)
+        assert len(model.priors) > 0
+        assert model.priors == model_set_up['priors']
+        assert 'intercept' in model.priors.keys()
+        assert 'variance' in model.priors.keys()
+        assert all(['mean' in regressor_data.keys() for regressor, regressor_data in model.priors.items()
                     if regressor != 'variance'])
-        assert all(['variance' in regressor_data.keys() for regressor, regressor_data in empty_model.priors.items()
+        assert all(['variance' in regressor_data.keys() for regressor, regressor_data in model.priors.items()
                     if regressor != 'variance'])
-        assert 'shape' in empty_model.priors['variance'].keys()
-        assert 'scale' in empty_model.priors['variance'].keys()
-        assert empty_model.variable_names is not None
-        assert empty_model.variable_names[0] == 'intercept'
-        assert 'variance' in empty_model.variable_names
+        assert 'shape' in model.priors['variance'].keys()
+        assert 'scale' in model.priors['variance'].keys()
+        assert model.variable_names is not None
+        assert model.variable_names[0] == 'intercept'
+        assert 'variance' in model.variable_names
 
 
     @mark.error
@@ -88,16 +168,20 @@ class TestModelPosteriors:
 
 
     @mark.genuine
-    def test_property(self, empty_model, general_testing_data):
-        posteriors = {variable: np.zeros((general_testing_data['n_iterations'], general_testing_data['n_chains']))
-                      for variable in general_testing_data['priors'].keys()}
-        empty_model.posteriors = posteriors
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_method(self, model_set_up):
+        model = LinearModel()
+        model.posteriors = model_set_up['posteriors']
 
-        assert empty_model.posteriors == posteriors
-        assert 'intercept' in empty_model.posteriors.keys()
-        assert 'variance' in empty_model.posteriors.keys()
-        assert all([posterior_samples.shape == (general_testing_data['n_iterations'], general_testing_data['n_chains'])
-                    for posterior_samples in empty_model.posteriors.values()])
+        assert isinstance(model.posteriors, dict)
+        assert len(model.posteriors) > 0
+        assert model.posteriors == model_set_up['posteriors']
+        assert 'intercept' in model.posteriors.keys()
+        assert 'variance' in model.posteriors.keys()
+        assert all([posterior_samples.shape == (model_set_up['n_samples'], model_set_up['n_chains'])
+                    for posterior_samples in model.posteriors.values()])
+        assert (model.posteriors['variance'] > 0).all()
 
 
     @mark.error
@@ -123,16 +207,21 @@ class TestModelPosteriorsToFrame:
 
 
     @mark.genuine
-    def test_method(self, empty_model, general_testing_data):
-        posteriors = {variable: np.zeros((general_testing_data['n_iterations'], general_testing_data['n_chains']))
-                      for variable in general_testing_data['priors'].keys()}
-        empty_model.posteriors = posteriors
-        posteriors_frame = empty_model.posteriors_to_frame()
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_method(self, model_set_up):
+        model = LinearModel()
+        model.posteriors = model_set_up['posteriors']
+        posteriors_frame = model.posteriors_to_frame()
 
         assert isinstance(posteriors_frame, pd.DataFrame)
         assert not posteriors_frame.empty
-        assert all(posteriors_frame.columns == list(posteriors.keys()))
-        assert len(posteriors_frame) == general_testing_data['n_iterations']*general_testing_data['n_chains']
+        assert all(posteriors_frame.columns == list(model_set_up['posteriors'].keys()))
+        assert len(posteriors_frame) == model_set_up['n_samples']*model_set_up['n_chains']
+        for col in model.posteriors.keys():
+            for i in range(model_set_up['n_samples']):
+                for j in range(model_set_up['n_chains']):
+                    assert model.posteriors[col][i, j] == posteriors_frame.loc[i*model_set_up['n_chains'] + j, col]
 
 
     @mark.error
@@ -146,19 +235,25 @@ class TestModelResiduals:
 
 
     @mark.genuine
-    def test_method(self, solved_model, general_testing_data):
-        residuals = solved_model.residuals()
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_method(self, model_set_up):
+        model = LinearModel()
+        model.data = model_set_up['data']
+        model.response_variable = model_set_up['response_variable']
+        model.posteriors = model_set_up['posteriors']
+        residuals = model.residuals()
 
         assert isinstance(residuals, pd.DataFrame)
         assert not residuals.empty
         assert 'predicted' in residuals.columns
         assert 'residuals' in residuals.columns
-        assert len(residuals) == len(general_testing_data['data'])
+        assert len(residuals) == len(model_set_up['data'])
         cols = list(residuals.columns)
         cols.remove('intercept')
         cols.remove('predicted')
         cols.remove('residuals')
-        assert set(cols) == set(general_testing_data['data'].columns)
+        assert set(cols) == set(model_set_up['data'].columns)
 
 
     @mark.error
@@ -172,14 +267,17 @@ class TestModelPredictDistribution:
 
 
     @mark.genuine
-    def test_method(self, solved_model, general_testing_data):
-        predictors = {predictor: 1 for predictor in general_testing_data['priors'].keys()
-                      if predictor not in ['intercept', 'variance']}
-        predicted = solved_model.predict_distribution(predictors = predictors)
+    @given(model_set_up())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_method(self, model_set_up):
+        if len(model_set_up['predictors']) != 0:
+            model = LinearModel()
+            model.posteriors = model_set_up['posteriors']
+            predicted = model.predict_distribution(predictors = model_set_up['predictors'])
 
-        assert isinstance(predicted, np.ndarray)
-        assert predicted.size != 0
-        assert len(predicted) == general_testing_data['n_iterations']*general_testing_data['n_chains']
+            assert isinstance(predicted, np.ndarray)
+            assert predicted.size != 0
+            assert len(predicted) == model_set_up['n_samples']*model_set_up['n_chains']
 
 
     @mark.error
@@ -207,14 +305,18 @@ class TestModelLikelihood:
 
 
     @mark.genuine
-    def test_method(self, complete_model):
-        data = pd.DataFrame({complete_model.response_variable: [0, 1, 2, 3, 4],
-                             'mean': [0, 1, 2, 3, 4],
-                             'variance': [1, 2, 3, 4, 5]})
-        likelihood = complete_model.likelihood(data = data)
+    @given(likelihood_data())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_method(self, likelihood_data):
+        model = LinearModel()
+        model.response_variable = 'y'
+        likelihood_data['y'] = np.random.uniform(low = likelihood_data['mean'].min(),
+                                                 high = likelihood_data['mean'].max(),
+                                                 size = len(likelihood_data))
+        likelihood = model.likelihood(data = likelihood_data)
 
         assert isinstance(likelihood, np.ndarray)
-        assert len(likelihood) == len(data)
+        assert len(likelihood) == len(likelihood_data)
 
 
     @mark.error
@@ -234,14 +336,18 @@ class TestModelLogLikelihood:
 
 
     @mark.genuine
-    def test_method(self, complete_model):
-        data = pd.DataFrame({complete_model.response_variable: [0, 1, 2, 3, 4],
-                             'mean': [0, 1, 2, 3, 4],
-                             'variance': [1, 2, 3, 4, 5]})
-        log_likelihood = complete_model.log_likelihood(data = data)
+    @given(likelihood_data())
+    @settings(max_examples = 20, deadline = None, suppress_health_check = [HealthCheck.data_too_large])
+    def test_method(self, likelihood_data):
+        model = LinearModel()
+        model.response_variable = 'y'
+        likelihood_data['y'] = np.random.uniform(low = likelihood_data['mean'].min(),
+                                                 high = likelihood_data['mean'].max(),
+                                                 size = len(likelihood_data))
+        log_likelihood = model.log_likelihood(data = likelihood_data)
 
         assert isinstance(log_likelihood, np.ndarray)
-        assert len(log_likelihood) == len(data)
+        assert len(log_likelihood) == len(likelihood_data)
 
 
     @mark.error
