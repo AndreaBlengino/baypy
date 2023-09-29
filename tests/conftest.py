68f9975c2f440e398ecd90cfbbe234e4ee161269
Andrea Blengino <ing.andrea.blengino@gmail.com>
2023-09-29 23:50:01 +0200

    TST: update tests according to LinearRegression modifications

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import baypy as bp
+from baypy.regression import LinearRegression
 from hypothesis.strategies import composite, integers, floats, lists
 import numpy as np
 import pandas as pd
@@ -101,8 +102,7 @@ model.priors = priors = {'x_1': {'mean': 0,
                                        'variance': 1e6},
                          'variance': {'shape': 1,
                                       'scale': 1e-6}}
-sampler = bp.regression.LinearRegression(model = model)
-sampler.sample(n_iterations = 500, burn_in_iterations = 50, n_chains = 3)
+LinearRegression.sample(model = model, n_iterations = 500, burn_in_iterations = 50, n_chains = 3)
 
 
 types_to_check = ['string', 2, 2.2, True, (0, 1), [0, 1], {0, 1}, {0: 1}, None,
@@ -212,98 +212,94 @@ def model_log_likelihood_value_error(request):
     return request.param
 
 
-@fixture(params = [type_to_check for type_to_check in types_to_check if not isinstance(type_to_check, bp.model.Model)])
-def linear_regression_init_type_error(request):
-    return request.param
-
-
-linear_regression_init_value_error_1 = bp.model.LinearModel()
-linear_regression_init_value_error_1.priors = {'x': {'mean': 0,
-                                                     'variance': 1},
-                                               'intercept': {'mean': 0,
-                                                             'variance': 1},
-                                               'variance': {'shape': 1,
-                                                            'scale': 1}}
-
-linear_regression_init_value_error_2 = bp.model.LinearModel()
-linear_regression_init_value_error_2.data = pd.DataFrame(columns = ['x', 'y', 'z'], index = [0])
-linear_regression_init_value_error_2.priors = {'x': {'mean': 0,
-                                                     'variance': 1},
-                                               'y': {'mean': 0,
-                                                     'variance': 1},
-                                               'intercept': {'mean': 0,
-                                                             'variance': 1},
-                                               'variance': {'shape': 1,
-                                                            'scale': 1}}
-
-linear_regression_init_value_error_3 = bp.model.LinearModel()
-linear_regression_init_value_error_3.data = pd.DataFrame(columns = ['x', 'z'], index = [0])
-linear_regression_init_value_error_3.response_variable = 'z'
-
-linear_regression_init_value_error_4 = bp.model.LinearModel()
-linear_regression_init_value_error_4.data = pd.DataFrame(columns = ['x', 'y', 'z'], index = [0])
-linear_regression_init_value_error_4.response_variable = 'w'
-linear_regression_init_value_error_4.priors = {'x': {'mean': 0,
-                                                     'variance': 1},
-                                               'y': {'mean': 0,
-                                                     'variance': 1},
-                                               'intercept': {'mean': 0,
-                                                             'variance': 1},
-                                               'variance': {'shape': 1,
-                                                            'scale': 1}}
-
-linear_regression_init_value_error_5 = bp.model.LinearModel()
-linear_regression_init_value_error_5.data = pd.DataFrame(columns = ['x', 'y', 'z'], index = [0])
-linear_regression_init_value_error_5.response_variable = 'z'
-linear_regression_init_value_error_5.priors = {'x': {'mean': 0,
-                                                     'variance': 1},
-                                               'y': {'mean': 0,
-                                                     'variance': 1},
-                                               'w': {'mean': 0,
-                                                     'variance': 1},
-                                               'intercept': {'mean': 0,
-                                                             'variance': 1},
-                                               'variance': {'shape': 1,
-                                                            'scale': 1}}
-
-@fixture(params = [linear_regression_init_value_error_1,
-                   linear_regression_init_value_error_2,
-                   linear_regression_init_value_error_3,
-                   linear_regression_init_value_error_4,
-                   linear_regression_init_value_error_5])
-def linear_regression_init_value_error(request):
-    return request.param
-
-
-linear_regression_sample_type_error_1 = [{'n_iterations': type_to_check, 'burn_in_iterations': 50,
+linear_regression_sample_type_error_1 = [{'model': type_to_check, 'n_iterations': 100, 'burn_in_iterations': 50,
                                           'n_chains': 3, 'seed': 137} for type_to_check in types_to_check
-                                         if not isinstance(type_to_check, int)]
+                                         if not isinstance(type_to_check, bp.model.Model)]
 
-linear_regression_sample_type_error_2 = [{'n_iterations': 100, 'burn_in_iterations': type_to_check,
-                                          'n_chains': 3, 'seed': 137} for type_to_check in types_to_check
-                                         if not isinstance(type_to_check, int)]
+linear_regression_sample_type_error_2 = [{'model': model, 'n_iterations': type_to_check,
+                                          'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137}
+                                         for type_to_check in types_to_check if not isinstance(type_to_check, int)]
 
-linear_regression_sample_type_error_3 = [{'n_iterations': 100, 'burn_in_iterations': 50,
-                                          'n_chains': type_to_check, 'seed': 137} for type_to_check in types_to_check
-                                         if not isinstance(type_to_check, int)]
+linear_regression_sample_type_error_3 = [{'model': model, 'n_iterations': 100,
+                                          'burn_in_iterations': type_to_check, 'n_chains': 3, 'seed': 137}
+                                         for type_to_check in types_to_check if not isinstance(type_to_check, int)]
 
-linear_regression_sample_type_error_4 = [{'n_iterations': 100, 'burn_in_iterations': 50,
+linear_regression_sample_type_error_4 = [{'model': model, 'n_iterations': 100,
+                                          'burn_in_iterations': 50, 'n_chains': type_to_check, 'seed': 137}
+                                         for type_to_check in types_to_check if not isinstance(type_to_check, int)]
+
+linear_regression_sample_type_error_5 = [{'model': model, 'n_iterations': 100, 'burn_in_iterations': 50,
                                           'n_chains': 3, 'seed': type_to_check} for type_to_check in types_to_check
                                          if not isinstance(type_to_check, int) and type_to_check is not None]
 
 @fixture(params = [*linear_regression_sample_type_error_1,
                    *linear_regression_sample_type_error_2,
                    *linear_regression_sample_type_error_3,
-                   *linear_regression_sample_type_error_4])
+                   *linear_regression_sample_type_error_4,
+                   *linear_regression_sample_type_error_5])
 def linear_regression_sample_type_error(request):
     return request.param
 
 
-@fixture(params = [{'n_iterations': -1, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
-                   {'n_iterations': 1000, 'burn_in_iterations': -1, 'n_chains': 3, 'seed': 137},
-                   {'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': -1, 'seed': 137},
-                   {'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': -1},
-                   {'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 2**32}])
+linear_regression_sample_value_error_1 = bp.model.LinearModel()
+linear_regression_sample_value_error_1.priors = {'x': {'mean': 0,
+                                                       'variance': 1},
+                                                 'intercept': {'mean': 0,
+                                                               'variance': 1},
+                                                 'variance': {'shape': 1,
+                                                              'scale': 1}}
+
+linear_regression_sample_value_error_2 = bp.model.LinearModel()
+linear_regression_sample_value_error_2.data = pd.DataFrame(columns = ['x', 'y', 'z'], index = [0])
+linear_regression_sample_value_error_2.priors = {'x': {'mean': 0,
+                                                       'variance': 1},
+                                                 'y': {'mean': 0,
+                                                       'variance': 1},
+                                                 'intercept': {'mean': 0,
+                                                               'variance': 1},
+                                                 'variance': {'shape': 1,
+                                                              'scale': 1}}
+
+linear_regression_sample_value_error_3 = bp.model.LinearModel()
+linear_regression_sample_value_error_3.data = pd.DataFrame(columns = ['x', 'z'], index = [0])
+linear_regression_sample_value_error_3.response_variable = 'z'
+
+linear_regression_sample_value_error_4 = bp.model.LinearModel()
+linear_regression_sample_value_error_4.data = pd.DataFrame(columns = ['x', 'y', 'z'], index = [0])
+linear_regression_sample_value_error_4.response_variable = 'w'
+linear_regression_sample_value_error_4.priors = {'x': {'mean': 0,
+                                                       'variance': 1},
+                                                 'y': {'mean': 0,
+                                                       'variance': 1},
+                                                 'intercept': {'mean': 0,
+                                                               'variance': 1},
+                                                 'variance': {'shape': 1,
+                                                              'scale': 1}}
+
+linear_regression_sample_value_error_5 = bp.model.LinearModel()
+linear_regression_sample_value_error_5.data = pd.DataFrame(columns = ['x', 'y', 'z'], index = [0])
+linear_regression_sample_value_error_5.response_variable = 'z'
+linear_regression_sample_value_error_5.priors = {'x': {'mean': 0,
+                                                       'variance': 1},
+                                                 'y': {'mean': 0,
+                                                       'variance': 1},
+                                                 'w': {'mean': 0,
+                                                       'variance': 1},
+                                                 'intercept': {'mean': 0,
+                                                               'variance': 1},
+                                                 'variance': {'shape': 1,
+                                                              'scale': 1}}
+
+@fixture(params = [{'model': linear_regression_sample_value_error_1, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
+                   {'model': linear_regression_sample_value_error_2, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
+                   {'model': linear_regression_sample_value_error_3, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
+                   {'model': linear_regression_sample_value_error_4, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
+                   {'model': linear_regression_sample_value_error_5, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
+                   {'model': model, 'n_iterations': -1, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 137},
+                   {'model': model, 'n_iterations': 1000, 'burn_in_iterations': -1, 'n_chains': 3, 'seed': 137},
+                   {'model': model, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': -1, 'seed': 137},
+                   {'model': model, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': -1},
+                   {'model': model, 'n_iterations': 1000, 'burn_in_iterations': 50, 'n_chains': 3, 'seed': 2**32}])
 def linear_regression_sample_value_error(request):
     return request.param
 
--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,25 +1,10 @@
 import baypy as bp
+from baypy.regression import LinearRegression
 from hypothesis import given, settings, HealthCheck
 import numpy as np
 import pandas as pd
 from pytest import mark, raises
 from tests.conftest import model_set_up, model
-
-
-@mark.regression
-class TestLinearRegressionInit:
-
-
-    @mark.genuine
-    def test_raises_type_error(self, linear_regression_init_type_error):
-        with raises(TypeError):
-            bp.regression.LinearRegression(model = linear_regression_init_type_error)
-
-
-    @mark.error
-    def test_raises_value_error(self, linear_regression_init_value_error):
-        with raises(ValueError):
-            bp.regression.LinearRegression(model = linear_regression_init_value_error)
 
 
 @mark.regression
@@ -34,11 +19,11 @@ class TestLinearRegressionSample:
         model.data = model_options['data']
         model.response_variable = model_options['response_variable']
         model.priors = model_options['priors']
-        sampler = bp.regression.LinearRegression(model = model)
-        sampler.sample(n_iterations = model_options['n_samples'],
-                       burn_in_iterations = model_options['burn_in_iterations'],
-                       n_chains = model_options['n_chains'],
-                       seed = model_options['seed'])
+        LinearRegression.sample(model = model,
+                                n_iterations = model_options['n_samples'],
+                                burn_in_iterations = model_options['burn_in_iterations'],
+                                n_chains = model_options['n_chains'],
+                                seed = model_options['seed'])
 
         assert isinstance(model.posteriors, dict)
         assert len(model.posteriors) > 0
@@ -65,19 +50,19 @@ class TestLinearRegressionSample:
 
     @mark.error
     def test_raises_type_error(self, linear_regression_sample_type_error):
-        sampler = bp.regression.LinearRegression(model = model)
         with raises(TypeError):
-            sampler.sample(n_iterations = linear_regression_sample_type_error['n_iterations'],
-                           burn_in_iterations = linear_regression_sample_type_error['burn_in_iterations'],
-                           n_chains = linear_regression_sample_type_error['n_chains'],
-                           seed = linear_regression_sample_type_error['seed'])
+            LinearRegression.sample(model = linear_regression_sample_type_error['model'],
+                                    n_iterations = linear_regression_sample_type_error['n_iterations'],
+                                    burn_in_iterations = linear_regression_sample_type_error['burn_in_iterations'],
+                                    n_chains = linear_regression_sample_type_error['n_chains'],
+                                    seed = linear_regression_sample_type_error['seed'])
 
 
     @mark.error
     def test_raises_value_error(self, linear_regression_sample_value_error):
-        sampler = bp.regression.LinearRegression(model = model)
         with raises(ValueError):
-            sampler.sample(n_iterations = linear_regression_sample_value_error['n_iterations'],
-                           burn_in_iterations = linear_regression_sample_value_error['burn_in_iterations'],
-                           n_chains = linear_regression_sample_value_error['n_chains'],
-                           seed = linear_regression_sample_value_error['seed'])
+            LinearRegression.sample(model = linear_regression_sample_value_error['model'],
+                                    n_iterations = linear_regression_sample_value_error['n_iterations'],
+                                    burn_in_iterations = linear_regression_sample_value_error['burn_in_iterations'],
+                                    n_chains = linear_regression_sample_value_error['n_chains'],
+                                    seed = linear_regression_sample_value_error['seed'])
