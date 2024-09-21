fb3c9f4949338837b04582ec4dba1e91677db0c6
Andrea Blengino <ing.andrea.blengino@protonmail.com>
2024-09-22 01:18:35 +0200

    ENH: enhance code style wrapping

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 import subprocess
 
 
-version = subprocess.run(['git', 'describe', '--tags'], stdout = subprocess.PIPE).stdout.decode('utf-8').strip()
-
+version = subprocess.run(
+    'git describe --tags'.split(),
+    stdout=subprocess.PIPE
+).stdout.decode('utf-8').strip()
 
 try:
-    setup(version = version)
+    setup(version=version)
 except Exception as e:
     if e.args[0].startswith("Invalid version: "):
-        setup(version = 'v0.0.0')
+        setup(version='v0.0.0')
     else:
         print(e)
